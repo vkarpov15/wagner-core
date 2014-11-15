889308920b44431a5d57434c62251bf9df0cecfe
Valeri Karpov <valeri.karpov@10gen.com>
2014-11-15 00:37:18 -0500

    Factories and more test coverage

--- test/basics.test.js
+++ test/basics.test.js
@@ -3,6 +3,10 @@ var assert = require('assert');
 var wagner = require('../');
 
 describe('core', function() {
+  afterEach(function() {
+    wagner.clear();
+  });
+
   it('works', function(done) {
     wagner.task('tristan', function(callback) {
       setTimeout(function() {
@@ -15,5 +19,102 @@ describe('core', function() {
       assert.equal(tristan, 'tristan');
       done();
     }, {});
+  });
+
+  it('error', function(done) {
+    wagner.task('tristan', function(callback) {
+      setTimeout(function() {
+        callback(null, 'tristan');
+      }, 50);
+    });
+
+    wagner.task('isolde', function(callback) {
+      setTimeout(function() {
+        callback('I got an error');
+      }, 25);
+    });
+
+    wagner.invoke(function(error, tristan, isolde) {
+      assert.equal(error, 'I got an error');
+      assert.ok(!tristan);
+      assert.ok(!isolde);
+      done();
+    });
+  });
+
+  it('sync', function(done) {
+    wagner.task('tristan', function() {
+      return 'tristan';
+    });
+
+    wagner.task('isolde', function() {
+      return 'isolde';
+    });
+
+    wagner.invoke(function(error, tristan, isolde) {
+      assert.ok(!error);
+      assert.equal(tristan, 'tristan');
+      assert.equal(isolde, 'isolde');
+      done();
+    });
+  });
+
+  it('recursive', function(done) {
+    wagner.task('tristan', function(callback) {
+      setTimeout(function() {
+        callback(null, 'tristan');
+      }, 50);
+    });
+
+    wagner.task('isolde', function(tristan, callback) {
+      setTimeout(function() {
+        callback(null, tristan + ' & isolde');
+      }, 25);
+    });
+
+    wagner.invoke(function(error, tristan, isolde) {
+      assert.ok(!error);
+      assert.equal(tristan, 'tristan');
+      assert.equal(isolde, 'tristan & isolde');
+      done();
+    });
+  });
+
+  it('without error', function(done) {
+    wagner.task('tristan', function(callback) {
+      setTimeout(function() {
+        callback(null, 'tristan');
+      }, 50);
+    });
+
+    wagner.task('isolde', function(tristan, callback) {
+      setTimeout(function() {
+        callback(null, tristan + ' & isolde');
+      }, 25);
+    });
+
+    wagner.invoke(function(tristan, isolde) {
+      assert.equal(tristan, 'tristan');
+      assert.equal(isolde, 'tristan & isolde');
+      done();
+    });
+  });
+
+  it('factory', function(done) {
+    wagner.factory('nothung', function() {
+      return { from: 'barnstokkr' };
+    });
+
+    wagner.task('sigfried', function(nothung, callback) {
+      setTimeout(function() {
+        callback(null, { sword: nothung });
+      }, 25);
+    });
+
+    wagner.invoke(function(error, sigfried) {
+      assert.ok(!error);
+      assert.equal(sigfried.sword.from, 'barnstokkr');
+      done();
+    });
   });
 });
